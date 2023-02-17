0dca006ccfb75548d58eee41d0bada864b9371a8
Ike Welborn <ike.welborn@gmail.com>
2023-02-17 18:02:29 +0000

    init commit

---
+++ bin/serve.ts
@@ -0,0 +1,4 @@
+#! /usr/bin/env node
+var shell = require('shelljs');
+
+shell.echo('Goodbye world!');
---
+++ dist/serve.js
@@ -0,0 +1,4 @@
+#! /usr/bin/env node
+"use strict";
+var shell = require('shelljs');
+shell.echo('Goodbye world!');
