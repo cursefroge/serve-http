3213a0167ac195a107bc188fdd8f3f7ae15b7887
Ike Welborn <ike.welborn@gmail.com>
2023-02-23 17:59:06 +0000

    Serve v1.0.0

--- dist/serve.js
+++ dist/serve.js
@@ -1,7 +1,38 @@
 #! /usr/bin/env node
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 const shell = require('shelljs');
 const yargs = require('yargs/yargs');
 const { hideBin } = require('yargs/helpers');
+const express = require("express");
+const helmet = require('helmet');
+const log4js = require('log4js');
 const argv = yargs(hideBin(process.argv)).argv;
-shell.echo(argv.level);
+const logger = log4js.getLogger("serve");
+// if we have a loglevel
+if (argv.loglevel) {
+    logger.level = argv.loglevel;
+}
+else {
+    logger.level = "info";
+}
+const app = express();
+const port = argv.port || 80;
+logger.debug("Initialized Express server");
+// if no path is specified, use current working directory. path is not an option, it comes directly after. "serve <path>"
+const path = argv._[0] || process.cwd();
+logger.debug('Serving ' + path);
+logger.info('Starting HTTP server on port ' + port);
+app.use((req, res, next) => {
+    logger.debug(req.method + ' ' + req.url);
+    next();
+});
+// use express.static in directory user is running command from
+app.use(express.static(path));
+if (argv.helmet) {
+    app.use(helmet());
+    logger.info('Helmet enabled');
+}
+app.listen(port, () => {
+    logger.info(`Server started`);
+});
