74c9a8aa72643380f73f29a89355a3ed0059c966
Ike Welborn <ike.welborn@gmail.com>
2023-02-23 18:46:49 +0000

    bugfixes

--- dist/serve.js
+++ dist/serve.js
@@ -9,7 +9,6 @@ const helmet = require('helmet');
 const log4js = require('log4js');
 const argv = yargs(hideBin(process.argv)).argv;
 const logger = log4js.getLogger("serve");
-// if we have a loglevel
 if (argv.loglevel) {
     logger.level = argv.loglevel;
 }
@@ -19,7 +18,6 @@ else {
 const app = express();
 const port = argv.port || 80;
 logger.debug("Initialized Express server");
-// if no path is specified, use current working directory. path is not an option, it comes directly after. "serve <path>"
 const path = argv._[0] || process.cwd();
 logger.debug('Serving ' + path);
 logger.info('Starting HTTP server on port ' + port);
@@ -27,7 +25,6 @@ app.use((req, res, next) => {
     logger.debug(req.method + ' ' + req.url);
     next();
 });
-// use express.static in directory user is running command from
 app.use(express.static(path));
 if (argv.helmet) {
     app.use(helmet());
