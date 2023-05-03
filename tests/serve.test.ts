08ec17d8c7899cf31a646e3f948f094bea0ee236
Ike Welborn <ike.welborn@gmail.com>
2023-05-03 13:05:36 +0000

    revive

--- tests/serve.test.ts
+++ tests/serve.test.ts
@@ -5,12 +5,10 @@ import http from 'http';
 // run serve command
 test('serve command', () => {
     // start the server, but terminate it after 5 seconds
-    const result = shell.exec('node bin/serve.js --loglevel debug', { async: true });
+    const result = shell.exec('node dist/bin/serve.js --loglevel debug', { async: true });
     setTimeout(() => {
         result.kill();
     }, 5000);
-    shell.echo(result.stdout);
-    shell.echo(result.stderr);
     //expect(result.code).toBe(0);
     //expect(result.stdout).toContain('Server started on port 80');
 });
